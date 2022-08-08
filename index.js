083cae030c36e7c93244dad16c3c6903ba7d63f4
Sounak Roy <100988764+sounakroy02@users.noreply.github.com>
2022-08-08 20:53:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// 1st Dice
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+// 2nd Dice
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png"; 
+var randomImageSource2 = "images/" + randomDiceImage2;
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+// Decision of Win Loss
+if (randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else if (randomNumber1<randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else
+{
+    document.querySelector("h1").innerHTML = "🚩 DRAW! 🚩";
+}
